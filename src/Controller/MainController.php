966e6f142a99e73ef9efddf2dccc485033c875ad
Hypno <hypno@hypno.hypno>
2021-10-13 12:30:50 +0200

    ajout photo de profile

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Entity\User;
 use App\Form\CreateArticleFormType;
+use App\Form\EditPhotoType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -37,6 +39,55 @@ class MainController extends AbstractController
     {
 
         return $this->render('main/profil.html.twig');
+    }
+    /**
+     * controller de la page de profil
+     *
+     * @Security("is_granted('ROLE_USER')")
+     * @Route("/edit-photo/", name="edit_photo")
+     */
+    public function photoEdit(Request $request): Response
+    {
+
+        $form = $this->createForm(EditPhotoType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $photo = $form->get('photo')->getData();
+
+            if ($this->getUser()->getPhoto() != null && file_exists($this->getParameter('app.user.photo.directory'). $this->getUser()->getPhoto())){
+
+                unlink( $this->getParameter('app.user.photo.directory') . $this->getUser()->getPhoto() );
+            }
+
+            do{
+                $newFileName = md5(random_bytes(100)).'.'. $photo->guessExtension();
+
+            }while(file_exists($this->getParameter('app.user.photo.directory'). $newFileName));
+
+
+            $this->getUser()->setPhoto($newFileName);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            $photo->move(
+                $this->getParameter('app.user.photo.directory'),
+                $newFileName
+            );
+            $this->addFlash('success', 'Batphoto modifier avec succés');
+
+            return $this->redirectToRoute('main_profil');
+        }
+
+
+
+
+        return $this->render('main/photo.html.twig',[
+            'form'=>$form->createView()
+        ]);
     }
 
 }
